2246b1f9b8d9805bd84bbad5a2989c78682d83ae
StrawHat-Luffyyy <krishmac1209@gmail.com>
2025-11-04 17:35:50 +0530

    feat: Update metrics controller to improve metrics aggregation and error handling
    - Refactored average response time calculation to use a more efficient aggregation method.
    - Added extraction of top error types with aggregation to identify the most frequent errors.
    - Enhanced response structure to include top error types in the metrics response.
    - Fixed a typo in the error logging statement for better debugging.

--- src/controllers/metrics.controller.js
+++ src/controllers/metrics.controller.js
@@ -4,15 +4,28 @@ export const getMetrics = async (req, res) => {
   try {
     const totalErrors = await ErrorLog.countDocuments();
 
-    const avgResponseTime = await ErrorLog.aggregate([
+    const avgResponse = await ErrorLog.aggregate([
+      { $group: { _id: null, avgTime: { $avg: "$responseTime" } } },
+    ]);
+    const avgResponseTime =
+      avgResponse[0]?.avgTime?.toFixed(2) || "No data yet";
+
+    //extract top error types
+    const topErrorTypes = await ErrorLog.aggregate([
       {
-        $group: {
-          _id: null,
-          avgTime: {
-            $avg: "$responseTime",
+        $project: {
+          type: {
+            $trim: {
+              input: {
+                $arrayElemAt: [{ $split: ["$input", ":"] }, 0],
+              },
+            },
           },
         },
       },
+      { $group: { _id: "$type", count: { $sum: 1 } } },
+      { $sort: { count: -1 } },
+      { $limit: 5 },
     ]);
     const topKeywords = await ErrorLog.aggregate([
       { $project: { words: { $split: ["$input", " "] } } },
@@ -26,14 +39,15 @@ export const getMetrics = async (req, res) => {
       .limit(7)
       .select("createdAt input");
 
-    res.json({
+    res.status(200).json({
       totalErrors,
-      avgResponseTime: avgResponseTime[0]?.avgTime?.toFixed(2) || "No data yet",
+      avgResponseTime,
+      topErrorTypes,
       topKeywords: topKeywords.map((k) => k._id),
       recentActivity,
     });
   } catch (error) {
-    onsole.error("Metrics error:", error);
+    console.error("Metrics error:", error);
     res.status(500).json({ message: "Error fetching metrics" });
   }
 };
